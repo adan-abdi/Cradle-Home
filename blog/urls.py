dd94d31718ae0bcf6409fc5cfd2574409340fbd7
Abdi-Adan <53440762+Abdi-Adan@users.noreply.github.com>
2020-05-18 10:42:16 +0300

    blog, migrations, superuser fix

--- blog/urls.py
+++ blog/urls.py
@@ -1,17 +1,11 @@
-from django.conf import settings
 from django.urls import path
-
 from . import views
 
-
-
-app_name = 'core'
+app_name = 'blog'
 
 urlpatterns = [
-    # path('', index, name='index'),
     # post views
     # path('', views.post_list, name='post_list'),
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
-
--- cradle/urls.py
+++ cradle/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
