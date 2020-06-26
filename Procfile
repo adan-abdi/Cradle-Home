03d14ac39b51588731d38ff88b68eef9d5f052e7
Abdi-Adan <53440762+Abdi-Adan@users.noreply.github.com>
2020-06-26 20:34:44 +0300

    heroku-push

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn zoneus.wsgi
+web: gunicorn cradle.wsgi
--- core/views.py
+++ core/views.py
@@ -2,21 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
 def home(request):
     context = {}
     return render(request, 'index.html', context)
-
 
 def timeline(request):
     context = {}
     return render(request, 'timeline.html', context)
 
-
 def blog(request):
     context = {}
     return render(request, 'blog-list.html', context)
-
 
 def blogdetail(request):
     context = {}
